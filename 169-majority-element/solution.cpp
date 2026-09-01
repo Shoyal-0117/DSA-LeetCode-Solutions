362564e4ad3d388d9f87e29e7452bcd8c8bb9c7d
Shoyal Haldar <shoyalhaldar.037@gmail.com>
2026-09-01 22:36:34 +0530

    Majority Element - 2026-09-01T17:06:33.317Z

---
+++ 169-majority-element/solution.cpp
@@ -0,0 +1,19 @@
+// 3 ms | 28.2 MB
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = 0, count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            } else if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+};
