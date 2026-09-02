9850c17ec55835c10d68b47004fb01d7efe8cc0a
Shoyal Haldar <shoyalhaldar.037@gmail.com>
2026-09-02 17:55:16 +0530

    Rotate Array - 2026-09-02T12:25:14.279Z

---
+++ 189-rotate-array/solution.cpp
@@ -0,0 +1,12 @@
+// 1 ms | 263.4 MB
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
