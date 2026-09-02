581a4efe26a17f4723d9b17fac1bc132eddf21cd
Shoyal Haldar <shoyalhaldar.037@gmail.com>
2026-09-02 16:23:48 +0530

    Search Insert Position - 2026-09-02T10:53:46.111Z

--- 35-search-insert-position/solution.cpp
+++ 35-search-insert-position/solution.cpp
@@ -1,24 +1,20 @@
-// 0 ms | 13.7 MB
+// 0 ms | 13.4 MB
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int start = 0;
-        int end = nums.size() - 1;
-        int mid = start + (end - start) / 2;
-
-        while (start <= end) {
-            if (nums[mid] == target) {
+        int end = nums.size() -1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(target == nums[mid]){
                 return mid;
-            } else if (nums[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
             }
-
-            mid = start + (end - start) / 2;
+            else if(target < nums[mid]){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
         }
-
         return start;
     }
-}
-;
+};
