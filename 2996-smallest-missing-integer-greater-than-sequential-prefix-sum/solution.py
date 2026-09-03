6078881a9c56123add3448114cbe14ca2decd4c8
Shoyal Haldar <shoyalhaldar.037@gmail.com>
2026-09-03 13:57:47 +0530

    Smallest Missing Integer Greater Than Sequential Prefix Sum - 2026-09-03T08:27:46.007Z

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/solution.py
@@ -0,0 +1,13 @@
+// 4 ms | 19.4 MB
+class Solution:
+    def missingInteger(self, nums: List[int]) -> int:
+        sum = nums[0]
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1] +1 :
+                sum += nums[i]
+            else:
+                break
+        while(sum in nums):
+            sum+=1
+        return sum
+        
