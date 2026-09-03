60955d03f963f1e8adfbb66756277cd6bb118b08
Shoyal Haldar <shoyalhaldar.037@gmail.com>
2026-09-03 10:00:17 +0530

    Counter - 2026-09-03T04:30:16.256Z

---
+++ 2620-counter/solution.js
@@ -0,0 +1,18 @@
+// 41 ms | 52 MB
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
