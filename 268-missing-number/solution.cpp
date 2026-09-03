cb7d58165bec9ff5f2026e81ea15e77bdf7ae389
Shoyal Haldar <shoyalhaldar.037@gmail.com>
2026-09-04 00:11:40 +0530

    Missing Number - 2026-09-03T18:41:39.425Z

--- 268-missing-number/solution.cpp
+++ 268-missing-number/solution.cpp
@@ -1,18 +1,17 @@
-// 4 ms | 21.8 MB
+// 0 ms | 21.8 MB
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-
-        int n = nums.size();
-        int xor_0_To_n = 0;
-
+    int Xor(int n){
+        int result = 0;
         switch(n % 4){
-            case 0 : xor_0_To_n = n; break;
-            case 1 : xor_0_To_n = 1; break;
-            case 2 : xor_0_To_n = n+1; break;
+            case 0 : result = n; break;
+            case 1 : result = 1; break;
+            case 2 : result = n+1; break;
             case 3 : break;
         }
-
-        return accumulate(nums.begin(),nums.end(),0,bit_xor<int>()) ^ xor_0_To_n;
+        return result;
+    }
+    int missingNumber(vector<int>& nums) {
+        return accumulate(nums.begin(),nums.end(),0,bit_xor<int>()) ^ Xor(nums.size());
     }
 };
