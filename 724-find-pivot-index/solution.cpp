b121c6ef813d417d4f883760315dd2bf793e503d
Shoyal Haldar <shoyalhaldar.037@gmail.com>
2026-09-05 21:13:10 +0530

    Find Pivot Index - 2026-09-05T15:43:08.088Z

--- 724-find-pivot-index/solution.cpp
+++ 724-find-pivot-index/solution.cpp
@@ -1,22 +1,19 @@
-// 0 ms | 36.3 MB
+// 0 ms | 35.6 MB
 class Solution {
 public:
-    vector<int> get_prefixSum(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> prefixsum(n + 1, 0);
-        for (int i = 0; i < n; i++)
-            prefixsum[i + 1] = prefixsum[i] + arr[i];
-        return prefixsum;
-    }
     int pivotIndex(vector<int>& nums) {
-        vector<int> prefixSumArr = get_prefixSum(nums);
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            int leftSum = prefixSumArr[i];
-            int rightSum = prefixSumArr[n] - prefixSumArr[i+1];
-            if (leftSum == rightSum) {
+        int totalSum = 0;
+        for (int x : nums)
+            totalSum += x; 
+
+        int leftSum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if (leftSum == rightSum)
                 return i;
-            }
+            leftSum += nums[i]; // ab next index ke liye leftSum update karo
         }
         return -1;
     }
