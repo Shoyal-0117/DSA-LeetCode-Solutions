726d57a70760caf50d23aa1be4bcf1a151a5e5ab
Shoyal Haldar <shoyalhaldar.037@gmail.com>
2026-09-13 23:26:04 +0530

    Move Zeroes - 2026-09-13T17:56:02.641Z

---
+++ 283-move-zeroes/solution.cpp
@@ -0,0 +1,13 @@
+// 0 ms | 23.8 MB
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[j] != 0) {
+                swap(nums[j], nums[i]);
+                i++;
+            }
+        }
+    }
+};
