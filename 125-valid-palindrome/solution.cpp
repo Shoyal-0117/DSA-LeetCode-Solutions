9dc70d6bcef94d17b136c883d882f27f7e8abcf8
Shoyal Haldar <shoyalhaldar.037@gmail.com>
2026-09-14 01:27:23 +0530

    Valid Palindrome - 2026-09-13T19:57:21.607Z

---
+++ 125-valid-palindrome/solution.cpp
@@ -0,0 +1,15 @@
+// 1 ms | 10.7 MB
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string filtered;
+        for (char c : s) {
+            if (isalnum(c)) {
+                filtered += tolower(c);
+            }
+        }
+        string rev = filtered;
+        reverse(rev.begin(), rev.end());
+        return filtered == rev;
+    }
+};
