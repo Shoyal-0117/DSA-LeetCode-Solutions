dc1260061430da83a25e6808b88230af49206ed7
Shoyal Haldar <shoyalhaldar.037@gmail.com>
2026-09-13 23:57:06 +0530

    Container With Most Water - 2026-09-13T18:27:05.248Z

---
+++ 11-container-with-most-water/solution.cpp
@@ -0,0 +1,19 @@
+// 0 ms | 62.9 MB
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int max_area = 0;
+        int l = 0;
+        int r = height.size() - 1;
+        while (l < r) {
+            int area = (r - l) * min(height[l], height[r]);
+            max_area = max(area, max_area);
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return max_area;
+    }
+};
