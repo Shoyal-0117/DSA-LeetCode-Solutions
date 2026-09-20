52e829717f5fd3828d6214581e86e67d1bdba8b2
Shoyal Haldar <shoyalhaldar.037@gmail.com>
2026-09-21 01:38:31 +0530

    Remove Duplicates from Sorted Array - 2026-09-20T20:08:30.094Z

--- 26-remove-duplicates-from-sorted-array/solution.cpp
+++ 26-remove-duplicates-from-sorted-array/solution.cpp
@@ -1,19 +1,8 @@
-// 0 ms | 22.6 MB
+// 0 ms | 22.5 MB
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
-        if (nums.empty()) return 0;
-
-        int k = 1;
-
-        for(int i=1; i < nums.size(); i++){
-            if ( nums[i] != nums[k-1] ){
-                nums[k] = nums[i];
-                k++;
-            }
-        }
-        
-        return k;
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+        return nums.size();
     }
 };
