8b64a2ef07599f5ff17fe58eaa98f4adf4d0194d
Shoyal Haldar <shoyalhaldar.037@gmail.com>
2026-09-21 18:22:28 +0530

    Matrix Diagonal Sum - 2026-09-21T12:52:27.754Z

---
+++ 1572-matrix-diagonal-sum/solution.cpp
@@ -0,0 +1,15 @@
+// 0 ms | 15 MB
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int sum = 0, j = mat.size() - 1;
+
+        for (int i = 0; i < mat.size(); i++) {
+            sum += mat[i][i];
+            if (i != j)
+                sum += mat[i][j];
+            j--;
+        }
+        return sum;
+    }
+};
